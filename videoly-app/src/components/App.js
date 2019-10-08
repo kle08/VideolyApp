e6e7a38559819abc1ccefd5fff77286aadb9b86e
iamasianman <kevinle1991@gmail.com>
2019-10-08 11:28:17 -0700

    Created Login form

--- videoly-app/src/components/App.js
+++ videoly-app/src/components/App.js
@@ -6,6 +6,7 @@ import Rentals from './rentals';
 import Customers from './customers';
 import NavBar from './navBar';
 import MovieForm from './movieForm';
+import LoginForm from './loginForm';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
         <NavBar />
         <main className='container'>
           <Switch>
+            <Route path='/login' component={LoginForm} />
             <Route path='/movies/:id' component={MovieForm} />
             <Route path='/movies' component={Movies}></Route>
             <Route path='/customers' component={Customers}></Route>
--- videoly-app/src/index.js
+++ videoly-app/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
