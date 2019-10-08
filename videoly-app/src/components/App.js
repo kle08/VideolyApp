d53d4b3888932dd9a177eb286df39eb357e85834
iamasianman <kevinle1991@gmail.com>
2019-10-07 20:27:44 -0700

    Implemented routes

--- videoly-app/src/components/App.js
+++ videoly-app/src/components/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import Movies from './Movies';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import NotFound from './notFound';
+import Rentals from './rentals';
+import Customers from './customers';
+import NavBar from './navBar';
+import MovieForm from './movieForm';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Movies />
-      </div>
+      <>
+        <NavBar />
+        <main className='container'>
+          <Switch>
+            <Route path='/movies/:id' component={MovieForm} />
+            <Route path='/movies' component={Movies}></Route>
+            <Route path='/customers' component={Customers}></Route>
+            <Route path='/rentals' component={Rentals}></Route>
+            <Route path='/not-found' component={NotFound}></Route>
+            <Redirect from='/' to='/movies' />
+            <Redirect to='/not-found' />
+          </Switch>
+        </main>
+      </>
     );
   }
 }
