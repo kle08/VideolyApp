536d1cd6373d78a95b95e02e39de9ffe78ecec54
iamasianman <kevinle1991@gmail.com>
2019-10-09 17:07:11 -0700

    Created Login Button

--- videoly-app/src/components/input.js
+++ videoly-app/src/components/input.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Input = ({ name, onChange, label, value }) => {
+const Input = ({ name, onChange, label, value, error }) => {
   return (
     <div className='form-group'>
       <label htmlFor={name}>{label}</label>
@@ -12,6 +12,7 @@ const Input = ({ name, onChange, label, value }) => {
         type='text'
         className='form-control'
       />
+      {error && <div className='alert alert-danger'>{error}</div>}
     </div>
   );
 };
